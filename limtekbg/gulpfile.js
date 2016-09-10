c711c911a1951650595eb1be94530e16d04967f5
Bilger Yahov <bilger@studyportals.com>
2016-09-10 22:09:21 +0200

    Set up gulpfile to keep track of the SCSS file changes.

--- limtekbg/gulpfile.js
+++ limtekbg/gulpfile.js
@@ -1,11 +1,22 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * @author Bilger Yahov <bayahov1@gmail.com>
+ * 
+ * All rights reserved. 2016.
+ * 
+ * @type Module gulp|Module gulp
  */
 
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
-gulp.task('default', function () {
-    // place code for your default task here
+gulp.task('styles', function(){
+
+	gulp.src('SCSS/*scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('CSS/'));
+});
+
+gulp.task('default', function(){
+
+     gulp.watch('SCSS/*.scss', ['styles']);
 });
