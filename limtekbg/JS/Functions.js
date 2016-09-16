b21bd42daae73c254eb2820189c11d628a072ca2
Bilger Yahov <bilger@studyportals.com>
2016-09-16 21:23:57 +0200

    Remove old version index page. Continue designing the index page. Implement toggle navigation.

--- limtekbg/JS/Functions.js
+++ limtekbg/JS/Functions.js
@@ -1,13 +1,46 @@
-/**
- * 
- */
-
 var MyFunctions = {
+    
+    navDemo: {},
+    toggleNavigation: {},
     
   init: function(){
       
       var $self = this;
       
+      $self.toggleNavigation = $('ToggleNavigation');
+      
+      if(!$self.toggleNavigation){
+          
+          console.error('MyFunctions.init(): ToggleNavigation is not found!');
+          return;
+      }
+      
+      $self.toggleNavigation.addEvent('click', function(){
+          
+         $self.toggleMenu(); 
+      });
+      
+  },
+  
+  toggleMenu: function(){
+      
+      var $self = this;
+      
+      $self.navDemo = $('NavDemo');
+      if(!$self.navDemo){
+          
+          console.error('MyFunctions.toggleMenu(): NavDemo is not found!');
+          return;
+      }
+      
+      if($self.navDemo.className.indexOf('w3-show') === -1){
+          
+          $self.navDemo.className += ' w3-show';
+      }
+      else{
+          
+          $self.navDemo.className = $self.navDemo.className.replace(' w3-show', '');
+      }
   }
 };
 
