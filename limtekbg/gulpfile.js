890d418a90632e17a020c38d60cce69e24d39867
Bilger Yahov <bilger@studyportals.com>
2017-01-20 13:54:35 +0100

    Modify gulpfile in such a way that now it has two different tasks for compiling JS ES6 to JS ES5 and SCSS to CSS.

--- limtekbg/gulpfile.js
+++ limtekbg/gulpfile.js
@@ -1,22 +1,30 @@
 /**
+ * @file gulpfile.js
+ *
  * @author Bilger Yahov <bayahov1@gmail.com>
- * 
- * All rights reserved. 2016.
- * 
- * @type Module gulp|Module gulp
+ * @version 1.0.0
+ * @copyright © 2017 Bilger Yahov, all rights reserved.
  */
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+// ES6 features only in strict mode.
+'use strict';
 
-gulp.task('styles', function(){
+let gulp  = require('gulp');
+let sass  = require('gulp-sass');
+let babel = require('gulp-babel');
+
+// Compile JS ES6 to JS ES5.
+gulp.task('javascript_compiler',  function(){
+
+	gulp.src('JS_ES6/*.js')
+		.pipe(babel())
+		.pipe(gulp.dest('public/JS/'));
+});
+
+// Compile SCSS to CSS.
+gulp.task('css_compiler', function(){
 
 	gulp.src('SCSS/*scss')
 		.pipe(sass().on('error', sass.logError))
 		.pipe(gulp.dest('public/CSS/'));
-});
-
-gulp.task('default', function(){
-
-     gulp.watch('SCSS/*.scss', ['styles']);
 });
