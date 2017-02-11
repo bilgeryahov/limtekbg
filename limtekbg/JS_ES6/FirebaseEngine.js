6529b9645503c44630c9d1e809318c55e9baa9d3
Bilger Yahov <bilger@studyportals.com>
2017-02-11 19:03:57 +0100

    Encapsulate the logic of FirebaseEngine.

--- limtekbg/JS_ES6/FirebaseEngine.js
+++ limtekbg/JS_ES6/FirebaseEngine.js
@@ -8,145 +8,170 @@
  * @copyright © 2016 - 2017 Bilger Yahov, all rights reserved.
  */
 
-const FirebaseEngine = {
+const FirebaseEngine = (function(){
 
-	_storage: {},
-	_storageRef: {},
+	/*
+	 * Encapsulate the logic.
+	 */
 
-	_database: {},
-	_databaseRef: {},
+	const Logic = {
 
-    /**
-	 * Initializes the Firebase engine.
-	 *
-	 * @return void
-     */
+        _storage: {},
+        _storageRef: {},
 
-	init: function(){
+        _database: {},
+        _databaseRef: {},
 
-		const $self = this;
+        /**
+         * Initializes the Firebase engine.
+         *
+         * @return void
+         */
 
-		let $config = {
-			apiKey: "AIzaSyDiXx5uys83vk-0bKdyKEfbExpZzdoChZE",
-			authDomain: "limtek-fb748.firebaseapp.com",
-			databaseURL: "https://limtek-fb748.firebaseio.com",
-			storageBucket: "limtek-fb748.appspot.com",
-			messagingSenderId: "876594439093"
-		};
+        init: function(){
 
-		firebase.initializeApp($config);
+            const $self = this;
 
-		// Initialize the storage.
-		$self._storage = firebase.storage();
-		$self._storageRef = $self._storage.ref();
+            let $config = {
+                apiKey: "AIzaSyDiXx5uys83vk-0bKdyKEfbExpZzdoChZE",
+                authDomain: "limtek-fb748.firebaseapp.com",
+                databaseURL: "https://limtek-fb748.firebaseio.com",
+                storageBucket: "limtek-fb748.appspot.com",
+                messagingSenderId: "876594439093"
+            };
 
-		// Initialize the real-time database.
-		$self._database = firebase.database();
-		$self._databaseRef = $self._database.ref();
-	},
+            firebase.initializeApp($config);
 
-    /**
-	 * Makes a GET request to the Firebase Real Time database.
-	 *
-     * @param $path - path to retrieve data from
-     * @param $extra - extra parameters provided
-     * @param $callback - execute after completing the request
-	 *
-	 * @return void
-     */
+            // Initialize the storage.
+            $self._storage = firebase.storage();
+            $self._storageRef = $self._storage.ref();
 
-	firebaseGET: function($path, $extra, $callback){
+            // Initialize the real-time database.
+            $self._database = firebase.database();
+            $self._databaseRef = $self._database.ref();
+        },
 
-		const $self = this;
+        /**
+         * Makes a GET request to the Firebase Real Time database.
+         *
+         * @param $path - path to retrieve data from
+         * @param $extra - extra parameters provided
+         * @param $callback - execute after completing the request
+         *
+         * @return void
+         */
 
-		// Represent the extra parameter as a string.
-		let $extraString = '';
+        firebaseGET: function($path, $extra, $callback){
 
-		for(let $member in $extra){
+            const $self = this;
 
-			if($extra.hasOwnProperty($member)){
+            // Represent the extra parameter as a string.
+            let $extraString = '';
 
-				$extraString += $member + '=' + $extra[$member] + '&';
-			}
-		}
+            for(let $member in $extra){
 
-		// Remove the last & character.
-		$extraString = $extraString.substring(0, $extraString.length-1);
+                if($extra.hasOwnProperty($member)){
 
-		let $request = new Request({
-			url: 'https://limtek-fb748.firebaseio.com/' + $path + '.json' + '?' + $extraString,
-			method: 'GET',
-			onSuccess: function($data){
+                    $extraString += $member + '=' + $extra[$member] + '&';
+                }
+            }
 
-				// If the data arrived is null, the function returns null as data as well.
+            // Remove the last & character.
+            $extraString = $extraString.substring(0, $extraString.length-1);
 
-				// Parse the data to a JSON object.
-				$data = JSON.parse($data);
+            let $request = new Request({
+                url: 'https://limtek-fb748.firebaseio.com/' + $path + '.json' + '?' + $extraString,
+                method: 'GET',
+                onSuccess: function($data){
 
-				/*
-				 * If there is 'shallow: true' in the $extra parameter
-				 * make sure that the :true after each property is turned
-				 * into {}.
-				 */
+                    // If the data arrived is null, the function returns null as data as well.
 
-				if($extra.hasOwnProperty('shallow') && $extra['shallow'] === true){
+                    // Parse the data to a JSON object.
+                    $data = JSON.parse($data);
 
-					for(let $turnMember in $data){
+					/*
+					 * If there is 'shallow: true' in the $extra parameter
+					 * make sure that the :true after each property is turned
+					 * into {}.
+					 */
 
-                        if($data.hasOwnProperty($turnMember)){
+                    if($extra.hasOwnProperty('shallow') && $extra['shallow'] === true){
 
-                            $data[$turnMember] = {};
+                        for(let $turnMember in $data){
+
+                            if($data.hasOwnProperty($turnMember)){
+
+                                $data[$turnMember] = {};
+                            }
                         }
                     }
-				}
 
-				// Call the callback with the processed data and null for errors.
-				return $callback(null, $data);
-			},
-			onFailure: function($error){
+                    // Call the callback with the processed data and null for errors.
+                    return $callback(null, $data);
+                },
+                onFailure: function($error){
 
-				// Log the error.
-				console.error('FirebaseEngine.firebaseGET(): ' + $error);
+                    // Log the error.
+                    console.error('FirebaseEngine.firebaseGET(): ' + $error);
 
-				// Call the callback with an error and null for data.
-				return $callback('Data for ' + $path + ' did not arrive because an error!', null);
-			}
-		});
-
-		// Fire the XHR request.
-		$request.send();
-	},
-
-    /**
-	 * Retrieves URL for a specific storage item.
-	 *
-     * @param $path
-     * @param $callback
-	 *
-	 * @return void
-     */
-
-	retrieveStorageItemURL($path, $callback){
-
-		const $self= this;
-
-		let $itemRef = $self._storageRef.child($path);
-
-		$itemRef.getDownloadURL()
-			.then(function($URL){
-
-				// Call the callback with the URL and null for errors.
-				return $callback(null, $URL);
-            })
-			.catch(function($error){
-
-				console.error('FirebaseEngine.downloadStorageItem(): ' + $error.code);
-
-				// Call the callback with the error code and null for URL.
-				return $callback('Data for ' + $path + ' did not arrive because an error!', null);
+                    // Call the callback with an error and null for data.
+                    return $callback('Data for ' + $path + ' did not arrive because an error!', null);
+                }
             });
+
+            // Fire the XHR request.
+            $request.send();
+        },
+
+        /**
+         * Retrieves URL for a specific storage item.
+         *
+         * @param $path
+         * @param $callback
+         *
+         * @return void
+         */
+
+        retrieveStorageItemURL($path, $callback){
+
+            const $self= this;
+
+            let $itemRef = $self._storageRef.child($path);
+
+            $itemRef.getDownloadURL()
+                .then(function($URL){
+
+                    // Call the callback with the URL and null for errors.
+                    return $callback(null, $URL);
+                })
+                .catch(function($error){
+
+                    console.error('FirebaseEngine.downloadStorageItem(): ' + $error.code);
+
+                    // Call the callback with the error code and null for URL.
+                    return $callback('Data for ' + $path + ' did not arrive because an error!', null);
+                });
+        }
+	};
+
+	return{
+
+		init(){
+
+			Logic.init();
+		},
+
+        firebaseGET($path, $extra, $callback){
+
+			Logic.firebaseGET($path, $extra, $callback);
+		},
+
+        retrieveStorageItemURL($path, $callback){
+
+        	Logic.retrieveStorageItemURL($path, $callback);
+        }
 	}
-};
+})();
 
 document.addEvent('domready', function(){
 
