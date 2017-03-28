a720e9c9d8c185314dcb890327499bf8583a0f0a
Bilger Yahov <bilger@studyportals.com>
2017-03-28 18:02:35 +0200

    Improve the logout flow.

--- limtekbg/JS_ES6/AdministrationController.js
+++ limtekbg/JS_ES6/AdministrationController.js
@@ -12,6 +12,10 @@ const AdministrationController = (function () {
 
     const Logic = {
 
+        // Page elements.
+        _pageContent: {},
+        _logoutButton: {},
+
         /**
          * Initializes the main functionality.
          *
@@ -20,8 +24,114 @@ const AdministrationController = (function () {
 
         init(){
 
-            alert('AdministrationController.init(): Initialized!');
-        }
+            const $self = this;
+
+            if(!FirebaseEngine){
+
+                console.error('AdministrationController.init(): FirebaseEngine is not present!');
+                return;
+            }
+
+            if(!$self.getPageElements()){
+
+                return;
+            }
+
+            FirebaseEngine.getCurrentUserFlag().removeEvents('present');
+            FirebaseEngine.getCurrentUserFlag().addEvent('present', function(){
+
+                // TODO: User is here
+                console.log('AdministrationController#CurrentUserFlag: user is here ' + FirebaseEngine.getCurrentUser().email);
+            });
+
+            FirebaseEngine.getCurrentUserFlag().removeEvents('not_present');
+            FirebaseEngine.getCurrentUserFlag().addEvent('not_present', function(){
+
+                console.log('AdministrationController#CurrentUserFlag: User is not here, redirecting...');
+                window.location.replace('./login.html');
+            });
+
+            /*
+             * Make sure that attaching the events to the DOM elements
+             * comes actually after attaching the USER presence events.
+             * Since you better not click the Logout Button before
+             * the program knows what to do :P
+             */
+
+            // After you successfully get the page elements, try to attach their events.
+            $self.attachPageElementsEvents();
+        },
+
+        /**
+         * Get the elements from the DOM.
+         * If something is missing return false, otherwise true.
+         *
+         * @return {boolean}
+         */
+
+        getPageElements(){
+
+            const $self = this;
+
+            $self._pageContent = $('PageContent');
+            if(!$self._pageContent){
+
+                console.error('AdministrationController.getPageElements(): PageContent is not found!');
+                return false;
+            }
+
+            $self._logoutButton = $('LogoutButton');
+            if(!$self._logoutButton){
+
+                console.error('AdministrationController.getPageElements(): LogoutButton is not found!');
+                return false;
+            }
+
+            return true;
+        },
+
+        /**
+         * Attaches the corresponding events to the DOM elements.
+         *
+         * @return void
+         */
+
+        attachPageElementsEvents(){
+
+            const $self = this;
+
+            $self._logoutButton.addEvent('click', function(){
+
+                FirebaseEngine.logout(function($error, $success){
+
+                    return $self.handleLogout($error, $success);
+                });
+            });
+        },
+
+        /**
+         * Handles the loging out.
+         *
+         * @param $error
+         * @param $success
+         *
+         * return void
+         */
+
+        handleLogout($error, $success){
+
+            if($error){
+
+                console.error('AdministrationController.handleLogout(): ' + $error);
+                alert($error);
+                return;
+            }
+
+            if($success){
+
+                console.log('AdministrationController.handleLogout(): logout was successful.');
+            }
+        },
     };
 
     return{
@@ -35,5 +145,6 @@ const AdministrationController = (function () {
 
 document.addEvent('domready', function(){
 
+    // Initialize the main functionality.
     AdministrationController.init();
 });
