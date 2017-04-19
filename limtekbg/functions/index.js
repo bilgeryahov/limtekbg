298420d45a74215f023152f7dcd824294a859caf
Bilger Yahov <bilger@studyportals.com>
2017-04-19 16:03:32 +0200

    Install nodemailer package to Firebase functions. Start with a function to send email messages.

--- limtekbg/functions/index.js
+++ limtekbg/functions/index.js
@@ -1,19 +1,40 @@
-const functions = require('firebase-functions');
+'use strict';
 
-exports.helloWorld = functions.https.onRequest((req, res) => {
+const functions  = require('firebase-functions');
+const nodemailer = require('nodemailer');
 
-    res
-        .status(200)
-        .json({
-            message:'Hello from my first function!'
-        });
-});
+exports.sendMail = functions.https.onRequest((req, res) => {
 
-exports.myAge = functions.https.onRequest((req, res) => {
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth:{
+            user: 'limteksender@gmail.com',
+            pass: 'limtek_sender'
+        }
+    });
 
-    res
-        .status(404)
-        .json({
-            message:'My age is not found mate!'
-        });
+    let mailOptions = {
+        from: '"Test Too" <testtoo@testtoo.com',
+        to: 'bayahov1@gmail.com',
+        subject:'test',
+        text:'I am testing this shit.'
+    };
+
+    transporter.sendMail(mailOptions, (err, data) => {
+
+        if(err){
+
+            res
+                .status(503)
+                .json({
+                    message:'Problem with sending the e-mail!'
+                });
+
+            return;
+        }
+
+        res
+            .status(201)
+            .json(data);
+    });
 });
