fcd653ed2e86b93b0b0e68fbc327c3b8f9d02284
Bilger Yahov <bilger@studyportals.com>
2017-04-19 17:15:16 +0200

    Make sure that checks are performed on the parameters received from the POST/PUT request for sending the email message.

--- limtekbg/functions/index.js
+++ limtekbg/functions/index.js
@@ -4,22 +4,55 @@ const functions  = require('firebase-functions');
 const nodemailer = require('nodemailer');
 
 exports.sendMail = functions.https.onRequest((req, res) => {
-
     const allowedMethods = [
         'POST',
         'PUT'
     ];
 
-    if(!allowedMethods.includes(req.method)){
+    const allowedParameters = [
+        'from_name',
+        'from_email',
+        'subject',
+        'text'
+    ];
 
+    if(!allowedMethods.includes(req.method)){
         res
             .status(405)
             .json({
                 message:'HTTP Method is not allowed!'
             });
-
         return;
     }
+
+    if(Object.keys(req.body).length !== allowedParameters.length){
+        res
+            .status(400)
+            .json({
+                message:'There is a problem with request parameters.'
+            });
+        return;
+    }
+
+    for(let parameter in req.body){
+        if(req.body.hasOwnProperty(parameter)){
+            let index = allowedParameters.indexOf(parameter);
+            if(index !== -1){
+                allowedParameters.splice(index, 1);
+            }
+        }
+    }
+
+    if(allowedParameters.length !== 0){
+        res
+            .status(400)
+            .json({
+                message:'There is a problem with request parameters.'
+            });
+        return;
+    }
+
+    // TODO: Check the req params for securiry issues.
 
     let transporter = nodemailer.createTransport({
         service: 'gmail',
@@ -29,28 +62,32 @@ exports.sendMail = functions.https.onRequest((req, res) => {
         }
     });
 
+    let mailFromName   = req.body.from_name;
+    let mailFromEmail  = req.body.from_email;
+    let mailSubject    = req.body.subject;
+    let mailText       = req.body.text;
+
     let mailOptions = {
-        from: '"Test Too" <testtoo@testtoo.com',
-        to: 'bayahov1@gmail.com',
-        subject:'test',
-        text:'I am testing this shit.'
+        to             : 'bayahov1@gmail.com',
+        from           : mailFromName + ' ' + mailFromEmail,
+        subject        : mailSubject,
+        text           : mailText + '\n\nMessage: Email has been sent from this user: ' + mailFromEmail
     };
 
     transporter.sendMail(mailOptions, (err, data) => {
-
         if(err){
-
             res
                 .status(503)
                 .json({
-                    message:'Problem with sending the e-mail!'
+                    message:'Problem with sending the e-mail.'
                 });
-
             return;
         }
 
         res
             .status(201)
-            .json(data);
+            .json({
+                message: 'E-mail sent.'
+            });
     });
 });
