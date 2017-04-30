2c3140c959511fb440fc8d5a818d054ae3c43c5d
Bilger Yahov <bilger@studyportals.com>
2017-04-30 20:06:20 +0200

    Implement reCAPTCHA in the sendMail cloud function.

--- limtekbg/functions/index.js
+++ limtekbg/functions/index.js
@@ -2,11 +2,12 @@
 
 const functions  = require('firebase-functions');
 const nodemailer = require('nodemailer');
+const querystring = require('querystring');
+const https = require('https');
+const http = require('http');
 
 exports.sendMail = functions.https.onRequest((req, res) => {
-
-    // TODO: This needs to be fixed or?
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://limtek-fb748.firebaseapp.com');
     res.header('Access-Control-Allow-Headers', 'X-Requested-With');
     res.header('Access-Control-Allow-Methods', 'PUT,POST');
 
@@ -17,7 +18,6 @@ exports.sendMail = functions.https.onRequest((req, res) => {
     ];
 
     if(req.method === 'OPTIONS'){
-
         res.
             status(200)
             .json();
@@ -25,6 +25,7 @@ exports.sendMail = functions.https.onRequest((req, res) => {
     }
 
     const allowedParameters = [
+        'recaptcha_response',
         'from_name',
         'from_email',
         'from_phone',
@@ -68,50 +69,123 @@ exports.sendMail = functions.https.onRequest((req, res) => {
         return;
     }
 
-    /*
-     * Until this point we verify that the needed parameters
-     * have been passed with the needed request type.
-     */
-
-    let mailFromName   = req.body.from_name;
-    let mailFromEmail  = req.body.from_email;
-    let mailFromPhone  = req.body.from_phone;
-    let mailSubject    = req.body.subject;
-    let mailText       = req.body.text;
-
-    // TODO: Check the req params for their data types (if they are correct).
-    // TODO: Check the req params for securiry issues.
-
-    let transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth:{
-            user: 'limteksender@gmail.com',
-            pass: 'limtek_sender'
-        }
+    // Let's verify reCAPTCHA.
+    const recaptchaPOSTdata = querystring.stringify({
+        'secret': '6LeNYx8UAAAAAN4l_zsbZN_7lLY10pESj1TAla0_',
+        'response': req.body.recaptcha_response
     });
 
-    let mailOptions = {
-        to             : 'bayahov1@gmail.com',
-        from           : mailFromName + ' ' + mailFromEmail,
-        subject        : mailSubject,
-        text           : mailText + '\n\nMessage: Email has been sent from this user: ' + mailFromEmail
-                                  + '\n\nMessage: Phone number of the sender: ' + mailFromPhone
+    const recaptchaPOSToptions = {
+        hostname: 'www.google.com',
+        path: '/recaptcha/api/siteverify',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': Buffer.byteLength(recaptchaPOSTdata)
+        }
     };
 
-    transporter.sendMail(mailOptions, (err, data) => {
-        if(err){
-            res
-                .status(503)
-                .json({
-                    error:'Problem with sending the e-mail.'
-                });
-            return;
-        }
+    const recaptchaPOSTreq = https.request(recaptchaPOSToptions, (recaptchaPOSTres) => {
+        let output = '';
+        recaptchaPOSTres.setEncoding('utf8');
 
-        res
-            .status(201)
-            .json({
-                message: 'E-mail sent.'
-            });
+        recaptchaPOSTres.on('data', (chunk) => {
+            output += chunk;
+        });
+
+        recaptchaPOSTres.on('end', () => {
+            try{
+                let obj = JSON.parse(output);
+
+                if(!obj.hasOwnProperty('success') || !obj.hasOwnProperty('challenge_ts') ||
+                    !obj.hasOwnProperty('hostname')){
+                    console.log(obj);
+                    res
+                        .status(503)
+                        .json({
+                            error:'reCAPTCHA did not reply with proper message. It misses a property.'
+                        });
+                    return;
+                }
+
+                if(obj['success'] === true && obj['hostname'].includes('limtek-fb748.firebaseapp.com')){
+                 return finishSendingMail();
+                }
+
+                console.log(obj);
+                res
+                    .status(400)
+                    .json({
+                        error: 'reCAPTCHA did not pass success and hostname checks.'
+                    });
+                return;
+            }
+            catch(exc){
+                console.log(exc);
+                res
+                    .status(503)
+                    .json({
+                        error:'reCAPTCHA did not reply with proper message. Failed while parsing it.'
+                    });
+                return;
+            }
+        });
     });
+
+    recaptchaPOSTreq.on('error', (e) => {
+        console.log(e);
+        res
+            .status(503)
+            .json({
+                error:'reCAPTCHA request went wrong'
+            });
+        return;
+    });
+
+    recaptchaPOSTreq.write(recaptchaPOSTdata);
+    recaptchaPOSTreq.end();
+
+    const finishSendingMail = function(){
+        let mailFromName   = req.body.from_name;
+        let mailFromEmail  = req.body.from_email;
+        let mailFromPhone  = req.body.from_phone;
+        let mailSubject    = req.body.subject;
+        let mailText       = req.body.text;
+
+        // TODO: Check the req params for their data types (if they are correct).
+        // TODO: Check the req params for securiry issues.
+
+        let transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth:{
+                user: 'limteksender@gmail.com',
+                pass: 'limtek_sender'
+            }
+        });
+
+        let mailOptions = {
+            to             : 'bayahov1@gmail.com',
+            from           : mailFromName + ' ' + mailFromEmail,
+            subject        : mailSubject,
+            text           : mailText + '\n\nMessage: Email has been sent from this user: ' + mailFromEmail
+            + '\n\nMessage: Phone number of the sender: ' + mailFromPhone
+        };
+
+        transporter.sendMail(mailOptions, (err, data) => {
+            if(err){
+                res
+                    .status(503)
+                    .json({
+                        error:'Problem with sending the e-mail.'
+                    });
+                return;
+            }
+
+            res
+                .status(201)
+                .json({
+                    message: 'E-mail sent.'
+                });
+        });
+    }
 });
