43dc23bed992353683ac8f765b3c7e0d8c9d4e1a
Bilger Yahov <bilger@studyportals.com>
2017-05-19 12:52:59 +0200

    Make sure that the new deploy script follows the rules.

--- limtekbg/gulpfile.js
+++ limtekbg/gulpfile.js
@@ -6,7 +6,6 @@
  * @copyright © 2017 Bilger Yahov, all rights reserved.
  */
 
-// ES6 features only in strict mode.
 'use strict';
 
 const gulp  = require('gulp');
@@ -14,25 +13,7 @@ const sass  = require('gulp-sass');
 const babel = require('gulp-babel');
 const run_sequence = require('run-sequence');
 const replace = require('gulp-replace');
-const exec = require('child_process').exec;
-const cofigFileLimtek = require('./configFileLimtek.json');
 const clean  = require('gulp-clean');
-
-// Compile JS ES6 to JS ES5.
-gulp.task('compile_javascript',  function(){
-
-	return gulp.src('./JS_ES6/*.js')
-		.pipe(babel())
-		.pipe(gulp.dest('./Deploy/JS/'));
-});
-
-// Compile SCSS to CSS.
-gulp.task('compile_css', function(){
-
-	return gulp.src('./SCSS/*scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('./Deploy/CSS/'));
-});
 
 // Clean the folder.
 gulp.task('clean_content', function(){
@@ -41,74 +22,38 @@ gulp.task('clean_content', function(){
         .pipe(clean());
 });
 
+// Clean the scss files.
+gulp.task('clean_scss', function(){
+
+    return gulp.src('./Deploy/**/*.scss', {read : false})
+        .pipe(clean());
+});
+
 // Copy static files.
 gulp.task('copy_content', function(){
 
-	return gulp.src('./Content/**')
-		.pipe(gulp.dest('./Deploy/'));
+    return gulp.src('./App/**')
+        .pipe(gulp.dest('./Deploy/'));
 });
 
-gulp.task('apply_development_api_key', function(){
+// Compile JS ES6 to JS ES5.
+gulp.task('compile_javascript',  function(){
 
-	return gulp.src('./Deploy/JS/FirebaseAuthenticationManager.js', {base: './'})
-		.pipe(replace(cofigFileLimtek.api_key_default, cofigFileLimtek.api_key_development))
-		.pipe(gulp.dest('./'));
+    return gulp.src('./Deploy/Modules/**/*.js')
+        .pipe(babel())
+        .pipe(gulp.dest('./Deploy/Modules/'));
 });
 
-gulp.task('remove_development_api_key', function(){
+// Compile SCSS to CSS.
+gulp.task('compile_css', function(){
 
-    return gulp.src('./Deploy/JS/FirebaseAuthenticationManager.js', {base: './'})
-        .pipe(replace(cofigFileLimtek.api_key_development, cofigFileLimtek.api_key_default))
-        .pipe(gulp.dest('./'));
-});
-
-gulp.task('apply_live_api_key', function(){
-
-    return gulp.src('./Deploy/JS/FirebaseAuthenticationManager.js', {base: './'})
-        .pipe(replace(cofigFileLimtek.api_key_default, cofigFileLimtek.api_key_live))
-        .pipe(gulp.dest('./'));
-});
-
-gulp.task('remove_live_api_key', function(){
-
-    return gulp.src('./Deploy/JS/FirebaseAuthenticationManager.js', {base: './'})
-        .pipe(replace(cofigFileLimtek.api_key_live, cofigFileLimtek.api_key_default))
-        .pipe(gulp.dest('./'));
-});
-
-/**
- * After Firebase deploys, make sure that you perform applying the dev api key again.
- * Also make sure that this task only deploys the hosting.
- *
- * Database rules and functions should be deployed only by Firebase native CLI.
- */
-
-gulp.task('firebase_deploy', function(){
-
-	return exec('firebase deploy --only hosting', function(err, stdout, stderr){
-
-		if(err){
-
-			console.error(err);
-            return run_sequence('remove_live_api_key', 'apply_development_api_key');
-		}
-
-		console.log(stdout);
-		console.log(stderr);
-		return run_sequence('remove_live_api_key', 'apply_development_api_key');
-    });
+    return gulp.src('./Deploy/Modules/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./Deploy/Modules/'));
 });
 
 // Deploy locally.
 gulp.task('deploy_locally', function(){
 
-	return run_sequence('clean_content', 'copy_content', 'compile_css', 'compile_javascript',
-		'apply_development_api_key');
-});
-
-// Deploy live.
-gulp.task('deploy_live', function(){
-
-    return run_sequence('clean_content', 'copy_content', 'compile_css', 'compile_javascript',
-		'apply_live_api_key', 'firebase_deploy');
+    return run_sequence('clean_content', 'copy_content', 'compile_css', 'compile_javascript', 'clean_scss');
 });
