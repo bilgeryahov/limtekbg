1d63644bc3167f578914b692c93f1b9187ee3f03
Bilger Yahov <bilger@studyportals.com>
2017-05-24 17:28:42 +0200

    Improve Login module.

--- limtekbg/App/Modules/Login/login.js
+++ limtekbg/App/Modules/Login/login.js
@@ -14,7 +14,16 @@ const Login = (function(){
 
         _templatePath: './Modules/Login/login.html',
         _placeholderName: 'LoginPlaceholder',
-        _templateFactory: null,
+        _flexibleTemplateFactory: null,
+
+        // Login's form elements
+        _elementsPresent: false,
+        _inputEmail: {},
+        _inputPassword: {},
+        _loginButton: {},
+
+        // Login's Auth Observer
+        _authObserver: {},
 
         /**
          * Initializes the main functionality.
@@ -26,11 +35,148 @@ const Login = (function(){
 
             const selfObj = this;
 
-            selfObj._templateFactory = new TemplateFactory(
+            if(!FirebaseAuthenticationManager){
+
+                console.error('Login.init() FirebaseAuthenticationManager is not present!');
+                return;
+            }
+
+            if(!CustomMessage){
+
+                console.error('Login.init(): CustomMessage module is not present!');
+                return;
+            }
+
+            if(!Loader){
+
+                console.error('Login.init(): Loader module is not present!');
+                return;
+            }
+
+            selfObj._flexibleTemplateFactory = new FlexibleTemplateFactory(
                 selfObj._templatePath, selfObj._placeholderName, {}
             );
 
-            selfObj._templateFactory.initProcess();
+            selfObj._flexibleTemplateFactory.initProcess();
+            selfObj._flexibleTemplateFactory.showPlaceholder();
+
+            // Create a new Observer for the Auth state.
+            selfObj._authObserver = new Observer();
+
+            // Set-up my Auth update settings.
+            selfObj._authObserver.getUpdate = function(update){
+
+                if(update === 'USER 1'){
+
+                    console.log('Login: User is here, redirecting....');
+                    window.location.replace('./administration.html');
+                }
+                else if(update === 'USER 0'){
+
+                    selfObj.displayLogin();
+                }
+                else if(update === 'ERROR 1'){
+
+                    // Problem while logging in!
+                    console.error('Login: ' + FirebaseAuthenticationManager.getAuthError());
+                    CustomMessage.showMessage(FirebaseAuthenticationManager.getAuthError());
+                    selfObj.displayLogin();
+                }
+            };
+
+            // Add my Auth Observer as an observer to FirebaseAuthenticationManager's ObserverManager.
+            FirebaseAuthenticationManager.getAuthObserverManager().addObserver(selfObj._authObserver);
+        },
+
+        /**
+         * Fetches all the Login Form elements from the DOM.
+         * If something goes wrong, false is returned. Otherwise true.
+         *
+         * @return {boolean}
+         */
+
+        getLoginFormElements(){
+
+            const selfObj = this;
+
+            if(selfObj._elementsPresent){
+
+                return true;
+            }
+
+            selfObj._loginButton = $('LoginButton');
+            selfObj._inputEmail = $('InputEmail');
+            selfObj._inputPassword = $('InputPassword');
+
+            selfObj._elementsPresent = (
+                selfObj._loginButton !== null
+                && selfObj._inputEmail !== null
+                && selfObj._inputPassword !== null
+            );
+
+            return selfObj._elementsPresent;
+        },
+
+        /**
+         * Starts the login process.
+         *
+         * @return void
+         */
+
+        attemptLogin(){
+
+            const selfObj = this;
+
+            // Try to fetch the elements from the DOM.
+            if(!selfObj.getLoginFormElements()){
+
+                console.error('Login.attemptLogin(): Problem with DOM elements.');
+                return;
+            }
+
+            // Validate.
+            if(
+                DevelopmentHelpers.validateCorrectness(selfObj._inputEmail.value, 'email')
+                && DevelopmentHelpers.validateCorrectness(selfObj._inputPassword.value, 'password')
+                && DevelopmentHelpers.validateSecurity(selfObj._inputEmail)
+                && DevelopmentHelpers.validateSecurity(selfObj._inputPassword)
+            ){
+
+                selfObj.displayLoader();
+                FirebaseAuthenticationManager.login(selfObj._inputEmail.value, selfObj._inputPassword.value);
+            }
+            else{
+
+                console.error('Login.attemptLogin(): Problem with validation of the input fields.');
+                CustomMessage.showMessage('Въвели сте некоректна информация или символи.');
+            }
+        },
+
+        /**
+         * Makes sure that the Login (form or module) is displayed when needed.
+         *
+         * @return void.
+         */
+
+        displayLogin(){
+
+            const selfObj = this;
+            selfObj._flexibleTemplateFactory.showPlaceholder();
+            Loader.hideMe();
+
+        },
+
+        /**
+         * Makes sure that the Loader is displayed when needed.
+         *
+         * @return void
+         */
+
+        displayLoader(){
+
+            const selfObj = this;
+            selfObj._flexibleTemplateFactory.hidePlaceholder();
+            Loader.showMe();
         }
     };
 
@@ -39,6 +185,11 @@ const Login = (function(){
         init(){
 
             Logic.init();
+        },
+
+        attemptLogin(){
+
+            Logic.attemptLogin();
         }
     }
 })();
