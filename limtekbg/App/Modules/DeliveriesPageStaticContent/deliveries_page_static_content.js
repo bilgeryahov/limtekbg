839bba4c7e02fad2bb425e9cc7fc3cab183f07bb
Bilger Yahov <bilger@studyportals.com>
2017-05-24 11:45:50 +0200

    Use the template factory.

--- limtekbg/App/Modules/DeliveriesPageStaticContent/deliveries_page_static_content.js
+++ limtekbg/App/Modules/DeliveriesPageStaticContent/deliveries_page_static_content.js
@@ -12,60 +12,25 @@ const DeliveriesPageStaticContent = (function(){
 
     const Logic = {
 
-        _template: null,
-        _placeholder: null,
         _templatePath: './Modules/DeliveriesPageStaticContent/deliveries_page_static_content.html',
+        _placeholderName: 'DeliveriesPageStaticContentPlaceholder',
+        _templateFactory: null,
 
         /**
-         * Gets the template and placeholder. If goes successful, calls
-         * for generating the template.
+         * Initializes the main functionality.
          *
          * @return void
          */
 
         init(){
 
-            if(!Handlebars){
-
-                console.error('DeliveriesPageStaticContent.init(): Handlebars is not present!');
-                return;
-            }
-
             const selfObj = this;
 
-            selfObj._template = $('DeliveriesPageStaticContentTemplate');
-            selfObj._placeholder = $('DeliveriesPageStaticContentPlaceholder');
+            selfObj._templateFactory = new TemplateFactory(
+                selfObj._templatePath, selfObj._placeholderName, {}
+            );
 
-            if(!selfObj._template || !selfObj._placeholder){
-
-                console.error('DeliveriesPageStaticContent.init(): Template Or Placeholder not found!');
-                return;
-            }
-
-            new Request({
-                url: selfObj._templatePath,
-                method: 'get',
-                onSuccess(data){
-                    return selfObj.generateTemplate(data);
-                },
-                onFailure(){
-                    console.error('DeliveriesPageStaticContent.init(): Failed while getting the template! Aborting!');
-                }
-            }).send();
-        },
-
-        /**
-         * Generates the template using Handlebars.
-         *
-         * @param data
-         *
-         * @return void
-         */
-
-        generateTemplate(data){
-            const selfObj = this;
-            const compiled = Handlebars.compile(data);
-            selfObj._placeholder.set('html', compiled());
+            selfObj._templateFactory.initProcess();
         }
     };
 
