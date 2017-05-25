f9eaab2910331ab33143ad50acd6cb625e96ed8c
Bilger Yahov <bilger@studyportals.com>
2017-05-25 19:14:54 +0200

    Obey the new Template function(class) rules.

--- limtekbg/App/Modules/Header/header.js
+++ limtekbg/App/Modules/Header/header.js
@@ -14,7 +14,7 @@ const Header = (function(){
 
         _templatePath: './Modules/Header/Header.html',
         _placeholderName: 'HeaderPlaceholder',
-        _templateFactory: null,
+        _template: null,
 
         /**
          * Initializes the main functionality.
@@ -26,11 +26,11 @@ const Header = (function(){
 
             const $self = this;
 
-            $self._templateFactory = new TemplateFactory(
+            $self._template = new Template(
                 $self._templatePath, $self._placeholderName, {}
             );
 
-            $self._templateFactory.initProcess();
+            $self._template.displayMain();
         }
     };
 
