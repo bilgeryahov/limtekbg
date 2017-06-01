4ba22454730170704a73cea96d305500b7f108a0
Bilger Yahov <bilger@studyportals.com>
2017-06-01 11:05:33 +0200

    Implement request for validation of the input.

--- limtekbg/functions/UserActions.js
+++ limtekbg/functions/UserActions.js
@@ -89,110 +89,178 @@ module.exports = {
             }
         }
 
+        // Make a request for correctness and escaping HTML.
         if(reqParamsProblem.error === ''){
-            // TODO: Check the req params for their data types (if they are correct).
-            if(5 === 6){
-                reqParamsProblem.error = 'The request parameters did not pass the correctness check.';
+            let reqParamsObj = {};
+            for(let parameter in req.body){
+                // Make sure not to send recaptcha response to the validation cheker.
+                if(req.body.hasOwnProperty(parameter) && parameter !== 'recaptcha_response'){
+                    reqParamsObj[parameter] = req.body[parameter];
+                }
             }
-        }
 
-        if(reqParamsProblem.error !== ''){
-            res
-                .status(400)
-                .json(reqParamsProblem);
-            return;
-        }
+            const validateInputPOSTdata = querystring.stringify(reqParamsObj);
 
-        /*
-         * Deal with reCAPTCHA.
-         */
+            const validateInputPOSToptions = {
+                hostname: 'us-central1-limtek-fb748.cloudfunctions.net',
+                path: '/validateInput',
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Content-Length': Buffer.byteLength(validateInputPOSTdata)
+                }
+            };
 
-        const recaptchaPOSTdata = querystring.stringify({
-            'secret': '6LeNYx8UAAAAAN4l_zsbZN_7lLY10pESj1TAla0_',
-            'response': req.body.recaptcha_response
-        });
+            const validateInputPOSTreq = https.request(validateInputPOSToptions, (validateInputPOSTres) => {
+                let output = '';
+                validateInputPOSTres.setEncoding('utf8');
+                validateInputPOSTres.on('data', (chunk) => {
+                    output += chunk;
+                });
 
-        const recaptchaPOSToptions = {
-            hostname: 'www.google.com',
-            path: '/recaptcha/api/siteverify',
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Content-Length': Buffer.byteLength(recaptchaPOSTdata)
-            }
-        };
-
-        const recaptchaPOSTreq = https.request(recaptchaPOSToptions, (recaptchaPOSTres) => {
-            let output = '';
-            recaptchaPOSTres.setEncoding('utf8');
-            recaptchaPOSTres.on('data', (chunk) => {
-                output += chunk;
-            });
-
-            recaptchaPOSTres.on('end', () => {
-                try{
-                    let obj = JSON.parse(output);
-
-                    if(!obj.hasOwnProperty('success') || !obj.hasOwnProperty('challenge_ts') ||
-                        !obj.hasOwnProperty('hostname')){
-                        console.error(obj);
+                validateInputPOSTres.on('end', () => {
+                    try{
+                        let obj = JSON.parse(output);
+                        if(obj.hasOwnProperty('error')){
+                            reqParamsProblem.error = obj.error;
+                        }
+                        return ableToContinue(obj);
+                    }
+                    catch(exc){
+                        console.error('Exception: ' + exc);
+                        console.error('Output: ' + output);
                         res
                             .status(503)
                             .json({
-                                error:'reCAPTCHA did not reply with proper message. It misses a property.'
+                                error:'Failed while parsing the validate input response object.'
                             });
-                        return;
                     }
-
-                    // TODO: Remove localhost from the hostname.
-                    if(obj['success'] === true &&
-                        (obj['hostname'].includes('limtek-fb748.firebaseapp.com') || (obj['hostname'].includes('localhost')))
-                    ){
-                        return finishSaving();
-                    }
-
-                    console.error(obj);
-                    res
-                        .status(400)
-                        .json({
-                            error: 'reCAPTCHA did not pass success and hostname checks.'
-                        });
-                }
-                catch(exc){
-                    console.error('Exception: ' + exc);
-                    console.error('Output: ' + output);
-                    res
-                        .status(503)
-                        .json({
-                            error:'reCAPTCHA did not reply with proper message. Failed while parsing it.'
-                        });
-                }
-            });
-        });
-
-        recaptchaPOSTreq.on('error', (e) => {
-            console.error(e);
-            res
-                .status(503)
-                .json({
-                    error:'reCAPTCHA request went wrong.'
                 });
-        });
+            });
 
-        recaptchaPOSTreq.write(recaptchaPOSTdata);
-        recaptchaPOSTreq.end();
+            validateInputPOSTreq.on('error', (e) => {
+                console.error(e);
+                res
+                    .status(503)
+                    .json({
+                        error:'Validate input request went wrong.'
+                    });
+            });
+
+            validateInputPOSTreq.write(validateInputPOSTdata);
+            validateInputPOSTreq.end();
+        }
+        else{
+            return ableToContinue({});
+        }
+
+        const ableToContinue = function (newBody) {
+            if(reqParamsProblem.error !== ''){
+                res
+                    .status(400)
+                    .json(reqParamsProblem);
+                return;
+            }
+
+            return continueReCAPTCHAprocess(newBody)
+        };
+
+        const continueReCAPTCHAprocess = function (newBody) {
+
+            // Add the recaptcha response to the newBody.
+            newBody.recaptcha_response = req.body.recaptcha_response;
+
+            /*
+             * Deal with reCAPTCHA.
+             */
+
+            const recaptchaPOSTdata = querystring.stringify({
+                'secret': '6LeNYx8UAAAAAN4l_zsbZN_7lLY10pESj1TAla0_',
+                'response': newBody.recaptcha_response
+            });
+
+            const recaptchaPOSToptions = {
+                hostname: 'www.google.com',
+                path: '/recaptcha/api/siteverify',
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Content-Length': Buffer.byteLength(recaptchaPOSTdata)
+                }
+            };
+
+            const recaptchaPOSTreq = https.request(recaptchaPOSToptions, (recaptchaPOSTres) => {
+                let output = '';
+                recaptchaPOSTres.setEncoding('utf8');
+                recaptchaPOSTres.on('data', (chunk) => {
+                    output += chunk;
+                });
+
+                recaptchaPOSTres.on('end', () => {
+                    try{
+                        let obj = JSON.parse(output);
+
+                        if(!obj.hasOwnProperty('success') || !obj.hasOwnProperty('challenge_ts') ||
+                            !obj.hasOwnProperty('hostname')){
+                            console.error(obj);
+                            res
+                                .status(503)
+                                .json({
+                                    error:'reCAPTCHA did not reply with proper message. It misses a property.'
+                                });
+                            return;
+                        }
+
+                        // TODO: Remove localhost from the hostname.
+                        if(obj['success'] === true &&
+                            (obj['hostname'].includes('limtek-fb748.firebaseapp.com') || (obj['hostname'].includes('localhost')))
+                        ){
+                            return finishSaving(newBody);
+                        }
+
+                        console.error(obj);
+                        res
+                            .status(400)
+                            .json({
+                                error: 'reCAPTCHA did not pass success and hostname checks.'
+                            });
+                    }
+                    catch(exc){
+                        console.error('Exception: ' + exc);
+                        console.error('Output: ' + output);
+                        res
+                            .status(503)
+                            .json({
+                                error:'reCAPTCHA did not reply with proper message. Failed while parsing it.'
+                            });
+                    }
+                });
+            });
+
+            recaptchaPOSTreq.on('error', (e) => {
+                console.error(e);
+                res
+                    .status(503)
+                    .json({
+                        error:'reCAPTCHA request went wrong.'
+                    });
+            });
+
+            recaptchaPOSTreq.write(recaptchaPOSTdata);
+            recaptchaPOSTreq.end();
+        };
 
         // POST request going to Database to save the message.
-        const finishSaving = function(){
+        const finishSaving = function(newBody){
 
             // TODO: hardcoded database path to be fixed later.
             const databasePath = '/development/messages/';
 
-            let mailFromName   = req.body.from_name;
-            let mailFromEmail  = req.body.from_email;
-            let mailFromPhone  = req.body.from_phone;
-            let mailSubject    = req.body.subject;
-            let mailText       = req.body.text;
+            let mailFromName   = newBody.from_name;
+            let mailFromEmail  = newBody.from_email;
+            let mailFromPhone  = newBody.from_phone;
+            let mailSubject    = newBody.subject;
+            let mailText       = newBody.text;
 
             const message = {
                 'sentOn': firebase.database.ServerValue.TIMESTAMP,
