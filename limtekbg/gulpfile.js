7f2a167b9d8b6e581f0f155c2d14e40356a09b01
Bilger Yahov <bilger@studyportals.com>
2017-06-19 14:13:51 +0200

    Implement choosing for the right keys on the deploy file.

--- limtekbg/gulpfile.js
+++ limtekbg/gulpfile.js
@@ -1,12 +1,6 @@
 /**
  * @file gulpfile.js
  *
- * File which exposes deploy functionality for the project. There are two main
- * procedures - live and development deploy. Both make sure that
- * they apply the correct API keys as well as database paths.
- * After a live deploy the procedure makes sure that
- * live settings are cleared up, so no affection.
- *
  * @author Bilger Yahov <bayahov1@gmail.com>
  * @version 1.0.0
  * @copyright © 2017 Bilger Yahov, all rights reserved.
@@ -91,47 +85,40 @@ gulp.task('deploy_live', function(){
         'set_live_environment', 'firebase_deploy');
 });
 
-// Set the correct keys and paths for dev env.
+// Set the correct keys for dev env.
 gulp.task('set_development_environment', function () {
 
     return gulp.src('./Deploy/JavaScript/EnvironmentHelper.js', { base : './' })
-        .pipe(replace(configFileLimtek.firebase.api_keys.default,configFileLimtek.firebase.api_keys.development))
-        .pipe(replace(configFileLimtek.firebase.db_paths.default, configFileLimtek.firebase.db_paths.development))
+        .pipe(replace(configFileLimtek.firebase.placeholder.apiKey,
+                        configFileLimtek.firebase.development.apiKey))
+        .pipe(replace(configFileLimtek.firebase.placeholder.authDomain,
+                        configFileLimtek.firebase.development.authDomain))
+        .pipe(replace(configFileLimtek.firebase.placeholder.databaseURL,
+            configFileLimtek.firebase.development.databaseURL))
+        .pipe(replace(configFileLimtek.firebase.placeholder.messagingSenderId,
+            configFileLimtek.firebase.development.messagingSenderId))
+        .pipe(replace(configFileLimtek.firebase.placeholder.projectId,
+            configFileLimtek.firebase.development.projectId))
+        .pipe(replace(configFileLimtek.firebase.placeholder.storageBucket,
+            configFileLimtek.firebase.development.storageBucket))
         .pipe(gulp.dest('./'));
 });
 
-// Set the correct keys and paths for live env.
+// Set the correct keys for live env.
 gulp.task('set_live_environment', function () {
 
     return gulp.src('./Deploy/JavaScript/EnvironmentHelper.js', { base : './' })
-        .pipe(replace(configFileLimtek.firebase.api_keys.default,configFileLimtek.firebase.api_keys.live))
-        .pipe(replace(configFileLimtek.firebase.db_paths.default, configFileLimtek.firebase.db_paths.live))
+        .pipe(replace(configFileLimtek.firebase.placeholder.apiKey,
+            configFileLimtek.firebase.live.apiKey))
+        .pipe(replace(configFileLimtek.firebase.placeholder.authDomain,
+            configFileLimtek.firebase.live.authDomain))
+        .pipe(replace(configFileLimtek.firebase.placeholder.databaseURL,
+            configFileLimtek.firebase.live.databaseURL))
+        .pipe(replace(configFileLimtek.firebase.placeholder.messagingSenderId,
+            configFileLimtek.firebase.live.messagingSenderId))
+        .pipe(replace(configFileLimtek.firebase.placeholder.projectId,
+            configFileLimtek.firebase.live.projectId))
+        .pipe(replace(configFileLimtek.firebase.placeholder.storageBucket,
+            configFileLimtek.firebase.live.storageBucket))
         .pipe(gulp.dest('./'));
-});
-
-// Clear live setting after live deployment.
-gulp.task('clear_live_environment', function () {
-
-    return gulp.src('./Deploy/JavaScript/EnvironmentHelper.js', { base : './' })
-        .pipe(replace(configFileLimtek.firebase.api_keys.live,configFileLimtek.firebase.api_keys.development))
-        .pipe(replace(configFileLimtek.firebase.db_paths.live, configFileLimtek.firebase.db_paths.development))
-        .pipe(gulp.dest('./'));
-});
-
-// Firebase hosting deploy. Makes sure that after successful execution
-// the live environment settings are cleared up.
-gulp.task('firebase_deploy', function(){
-
-    return exec('firebase deploy --only hosting', function(err, stdout, stderr){
-
-        if(err){
-
-            console.error(err);
-            return run_sequence('clear_live_environment');
-        }
-
-        console.log(stdout);
-        console.log(stderr);
-        return run_sequence('clear_live_environment');
-    });
 });
